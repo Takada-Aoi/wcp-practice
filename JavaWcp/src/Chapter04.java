32d36dc09bc131d8ca77aceb0da333d5536fb69f
高田碧 <takadaaoi@takadamidorinoMacBook-Pro.local>
2021-08-06 23:40:42 +0900

    [Java-practice3.4]

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,16 @@
+
+public class Chapter04 {
+	
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 2;
+		String str1;
+		str1 = "Aoi";
+		System.out.println(str1);
+		
+		str1 = "Happy!";
+		System.out.println(str1);
+		
+		System.out.println(LUCKY_NUMBER);
+	}
+
+}
