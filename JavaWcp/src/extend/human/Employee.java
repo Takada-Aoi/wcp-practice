7b69fff65e5453f1f547d7112a2bc3436fbdbc9b
Takada-Aoi <a.m.3m0221@gmail.com>
2021-08-15 21:39:28 +0900

    [Java-practice13-2]

--- JavaWcp/src/extend/human/Employee.java
+++ JavaWcp/src/extend/human/Employee.java
@@ -1,5 +1,19 @@
 package extend.human;
 
-public class Employee {
+public class Employee extends Human {
+	private String department;
+
+	private String department;
+
+	public Employee(String name, int age, String department) {
+		super(name, age);
+		this.department = department;
+	}
+
+	public String getEmployeeProfile() {
+		String profile = "年齢は" + this.age + "です。";
+		profile += "サラリーマンで、部署は" + this.department + "です。";
+		return profile;
+	}
 
 }
